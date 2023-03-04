fbf0aec50c4d094c60fd43f43a1a15a805c407e9
Iman Allahverdiyev <113292209+Imann7@users.noreply.github.com>
2023-03-04 17:18:44 +0100

    Update Program.cs

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,7 +1,7 @@
 using System;
 
-  class Program
-    {
+class Program
+{
     static void Main()
     {
 
@@ -20,24 +20,24 @@ using System;
         Console.WriteLine("Enter coordinate y of dot C: ");
         double yC = Convert.ToDouble(Console.ReadLine());
 
-        
+
         double side1 = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2));
-        double side2 = Math.Sqrt(Math.Pow(xC-xB,2) + Math.Pow(yC-yB,2));
-        double side3 = Math.Sqrt(Math.Pow(xC-xA,2) + Math.Pow(yC-yA,2));
- 
-        
-        Console.WriteLine($"Length of AB is:  \'{side1}\'");
+        double side2 = Math.Sqrt(Math.Pow(xC - xB, 2) + Math.Pow(yC - yB, 2));
+        double side3 = Math.Sqrt(Math.Pow(xC - xA, 2) + Math.Pow(yC - yA, 2));
+
+
+        Console.WriteLine($"\nLength of AB is:  \'{side1}\'");
         Console.WriteLine($"Length of BC is:  \'{side2}\'");
         Console.WriteLine($"Length of AC is:  \'{side3}\'");
 
 
-        if(side1 == side2  && side2 == side3)
+        if (side1 == side2 && side2 == side3)
         {
-            Console.WriteLine("Triangle is \'Equilateral\'");
+            Console.WriteLine("\nTriangle is \'Equilateral\'");
         }
         else
         {
-            Console.WriteLine("Triangle is NOT \'Equilateral\'");
+            Console.WriteLine("\nTriangle is NOT \'Equilateral\'");
         }
 
 
@@ -49,11 +49,11 @@ using System;
         {
             Console.WriteLine("Triangle is NOT \'Isosceles\'");
         }
-        
+
 
         double delta = 1e-15;
 
-        if (Math.Pow(side1,2) - (Math.Pow(side2, 2) + Math.Pow(side3, 2) ) <= delta || Math.Pow(side2, 2) - (Math.Pow(side3, 2) + Math.Pow(side1, 2)) <= delta || Math.Pow(side3, 2) - (Math.Pow(side1, 2) + Math.Pow(side2, 2)) <= delta) 
+        if (Math.Pow(side1, 2) - (Math.Pow(side2, 2) + Math.Pow(side3, 2)) <= delta || Math.Pow(side2, 2) - (Math.Pow(side3, 2) + Math.Pow(side1, 2)) <= delta || Math.Pow(side3, 2) - (Math.Pow(side1, 2) + Math.Pow(side2, 2)) <= delta)
         {
             Console.WriteLine("Triangle is \'Right\'");
         }
@@ -62,11 +62,11 @@ using System;
             Console.WriteLine("Triangle is NOT \'Right\'");
         }
 
-        
-        double perimeter = side1 + side2 + side3;
-        Console.WriteLine($"Perimeter is: \'{perimeter}\' ");
 
-        Console.Write("Parity numbers in range from 0 to triangle perimeter: ");
+        double perimeter = side1 + side2 + side3;
+        Console.WriteLine($"\nPerimeter is: \'{perimeter}\' ");
+
+        Console.Write("\nParity numbers in range from 0 to triangle perimeter: ");
         for (int i = 0; i <= perimeter; i += 2)
         {
             Console.Write($"\n{i}");
